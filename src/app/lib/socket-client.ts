9daa0573de3f7c8cd0717054c50d3ace7d3bde46
Paulo Castro <castro.paulo5.00@gmail.com>
2025-11-29 16:39:48 +0800

    finals

--- src/app/lib/socket-client.ts
+++ src/app/lib/socket-client.ts
@@ -1,11 +1,23 @@
+// import { io, Socket } from "socket.io-client";
+
+// let socket: Socket | null = null;
+
+// export function getSocket(): Socket {
+//   if (!socket) {
+//     socket = io("/", {
+//       path: "/api/socket"
+//     });
+//   }
+//   return socket;
+// }
 import { io, Socket } from "socket.io-client";
 
-let socket: Socket | null = null;
+let socket: Socket;
 
-export function getSocket(): Socket {
+export function getSocket() {
   if (!socket) {
-    socket = io("/", {
-      path: "/api/socket"
+    socket = io("http://localhost:3000", {
+      path: "/api/socket",
     });
   }
   return socket;
